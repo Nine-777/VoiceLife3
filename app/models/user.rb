8c754a4b44574618a898d1e3f497d65fe671d53b
KenHaya <hkearth777@gmail.com>
2022-06-28 21:56:34 +0900

    view整備

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, {presence: true}
+  validates :email, {presence: true, uniqueness: true}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+  has_many :comments
+  # mount_uploader :avatar, AvatarUploader 
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+      image_name = "999"
+    end
+  end
+
 end
