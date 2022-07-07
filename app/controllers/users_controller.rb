ad1c395f96f1573c4382327d9d23e1e9c85972b8
KenHaya <hkearth777@gmail.com>
2022-07-07 21:25:15 +0900

    検索バー位置修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,27 +49,7 @@ class UsersController < ApplicationController
   
   def login_form
   end
-  
-  def login
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      flash[:notice] = "ログインしました"
-      redirect_to posts_path
-    else
-      @error_message = "メールアドレスまたはパスワードが間違っています"
-      @email = params[:email]
-      @password = params[:password]
-      render("users/login_form")
-    end
-  end
-  
-  def logout
-    session[:user_id] = nil
-    flash[:notice] = "ログアウトしました"
-    redirect_to("/login")
-  end
-  
+    
   def likes
     @user = User.find_by(id: params[:id])
     @likes = Like.where(user_id: @user.id)
