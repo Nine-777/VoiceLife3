a8ef519f7f731865ba118fb9fa12621ed936e447
KenHaya <hkearth777@gmail.com>
2022-07-10 18:31:33 +0900

    annotate適用/top下部アイコン変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text(65535)
+#  file       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
 class Post < ApplicationRecord
   validates :content, {presence: true, length: {maximum: 140}}
   validates :user_id, {presence: true}
