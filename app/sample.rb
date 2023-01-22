e31914d6e78bb1d466dbe545c103b102feb3317b
KenHaya <hkearth777@gmail.com>
2023-01-22 15:57:18 +0900

    del: 不要なためReact関連のコード削除

---
+++ app/sample.rb
@@ -0,0 +1,26 @@
+puts 1 + 2
+
+a = "Hello, world"
+puts a
+
+b = "こんにちは"
+puts b
+
+def fizz_buzz(n)
+  if n % 3 = 0
+  "Fizz"
+  else
+    n.to_s
+  end
+end
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+
+def zero?
+  if n = 0
+    true
+  else
+    false
+  end
+end
